ec591a00e859992fa2ee189afda10b661d0d6748
wichayac <wichaya.c@kkumail.com>
2026-02-04 23:46:54 +0700

    Refactor: change songData to an array to support multiple songs

--- static/js/script.js
+++ static/js/script.js
@@ -19,9 +19,10 @@ const homeData = {
     }
 }
 
-const songData = {
-    title: "Same Blue",
-    jp: `気持ちの整理がつかないままの朝に 散らかったそれを鞄に詰め込んだ
+const songData = [
+    {
+        title: "Same Blue",
+        jp: `気持ちの整理がつかないままの朝に 散らかったそれを鞄に詰め込んだ
         やっぱり僕は あなたの前の僕は 渡したい言葉なんて渡せないまま
         1人の夜に中身を広げようとして 胸の奥がひりついてたまらなかった
         好きな想いが あなたを好きという想いが あまりにも嵩張ってたから
@@ -48,7 +49,7 @@ const songData = {
         青のまま濁って澄んで 大きな未熟さを背負って
         明日も息を切らしたい あなたの居る目まぐるしい世界で
         あなたという季節の中で`,
-    en: `This morning when my thoughts
+        en: `This morning when my thoughts
         And my emotions were still a whole mess
         I have stuffed everything all into my bag
         No more, no less
@@ -107,7 +108,134 @@ const songData = {
         Tomorrow, I’ll still try to gasp for air
         In this spinning world that you live in without a single care
         We’re spending this season named under you as declared`
-}
+
+    },
+    {
+        title: "Pretender",
+        jp: `君とのラブストーリー
+        それは予想通り
+        いざ始まればひとり芝居だ
+        ずっとそばにいたって
+        結局ただの観客だ
+        感情のないアイムソーリー
+        それはいつも通り
+        慣れてしまえば悪くはないけど
+        君とのロマンスは人生柄
+        続きはしないことを知った
+        もっと違う設定で
+        もっと違う関係で
+        出会える世界線
+        選べたらよかった
+        もっと違う性格で
+        もっと違う価値観で
+        愛を伝えられたらいいな
+        そう願っても無駄だから
+        グッバイ
+        君の運命のヒトは僕じゃない
+        辛いけど否めない
+        でも離れ難いのさ
+        その髪に触れただけで
+        痛いや いやでも
+        甘いな いやいや
+        グッバイ
+        それじゃ僕にとって君は何？
+        答えは分からない
+        分かりたくもないのさ
+        たったひとつ確かなことがあるとするのならば
+        君は綺麗だ
+        誰かが偉そうに
+        語る恋愛の論理
+        何ひとつとしてピンとこなくて
+        飛行機の窓から見下ろした
+        知らない街の夜景みたいだ
+        もっと違う設定で
+        もっと違う関係で
+        出会える世界線
+        選べたらよかった
+        いたって純な心で
+        叶った恋を抱きしめて
+        好きだとか無責任に言えたらいいな
+        そう願っても虚しいのさ
+        グッバイ
+        繋いだ手の向こうにエンドライン
+        引き伸ばすたびに
+        疼きだす未来には
+        君はいない
+        その事実に Cry
+        そりゃ苦しいよな
+        グッバイ
+        君の運命のヒトは僕じゃない
+        辛いけど否めない
+        でも離れ難いのさ
+        その髪に触れただけで
+        痛いや いやでも
+        甘いな いやいや
+        グッバイ
+        それじゃ僕にとって君は何？
+        答えは分からない
+        分かりたくもないのさ
+        たったひとつ確かなことがあるとするのならば
+        君は綺麗だ
+        それもこれもロマンスの定めなら
+        悪くないよな
+        永遠も約束もないけれど
+        とても綺麗だ`,
+        en: `Our love story, just as expected
+        When it starts, it's just a one-man show
+        Even though I was always by your side, I'm just in the audience
+        An apology without emotion, just like always
+        It's not so bad once you get used to it
+
+        My romance with you is how life goes
+        I knew it would never last
+
+        I wish I had chosen a different setting, a different relation
+        A different world-line where I could meet you
+        I wish I had been able to show my love in a different personality
+        With different values
+        But I know it's pointless
+
+        Goodbye
+        I'm not your soulmate
+        It hurts, but I can't deny it, but it's still hard to let go
+        It hurts just to touch your hair, but
+        It feels so sweet, no no
+
+        Goodbye
+        So, what are you to me?
+        I don't know the answer, I don't want to know the answer
+        If there's just one thing I know that's true, it's that
+        "You're beautiful"
+
+        Some self-important person lecturing about love
+        But nothing makes any sense
+        It's like seeing the nightscape of an unknown city from an airplane window
+
+        I wish I had chosen a different setting, a different relation
+        A different world-line where I could meet you
+        I wish I could have had that love come true, and told you "I love you" without any responsibility
+        With a pure heart
+        But it's an empty wish
+
+        Goodbye
+        There's an end line beyond the hand I hold
+        Every time it's prolonged, at the end of a tingling future
+        You're not there, and I cry at that truth
+        It's just too painful
+
+        Goodbye
+        I'm not your soulmate
+        It hurts, but I can't deny it, but it's still hard to let go
+        It hurts just to touch your hair, but
+        It feels so sweet, no no
+
+        Goodbye
+        So, what are you to me?
+        I don't know the answer, I don't want to know the answer
+        If there's just one thing I know that's true, it's that
+        "You're beautiful"`
+    }
+];
 
 function toggleLanguage() {
     const targetLang = (currentLang === 'jp') ? 'en' : 'jp';
